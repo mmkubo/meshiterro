9972f154b80919b0d6b29a50da5f9ce93002743a
mmkubo <321625961+mmkubo@users.noreply.github.com>
2026-09-16 01:29:19 +0000

    [Finish]他のユーザーからのアクセス制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   allow_unauthenticated_access only: [:new, :create] 
   #認証をスキップ: サインアップ（new, create）はログイン前に行うため
@@ -36,4 +37,12 @@ private
   def user_params
     params.require(:user).permit(:name, :email_address, :password, :password_confirmation, :profile_image)
   end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == Current.user.id
+      redirect_to post_images_path
+    end
+  end
+
 end
